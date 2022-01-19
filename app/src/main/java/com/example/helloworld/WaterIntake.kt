0c6e38ad30d4ce294453945efbbf2be7fea1e59e
Rikcylolo <rikcypadang@gmail.com>
2022-01-19 09:49:49 +0700

    a

---
+++ app/src/main/java/com/example/helloworld/WaterIntake.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterIntake : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_intake)
+    }
+}
